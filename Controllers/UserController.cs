3da638e8948ddeb4c070a446072898b8d0be023d
Maks <elohhh2@gmail.com>
2024-11-13 02:29:12 +0200

    Implemented user info display and color customization based on query parameters
    1. Created an ASP.Net Core project with MVC support.
    2. Added controller logic to handle GET requests with a `name` parameter. For three fixed values of `name`, the application displays corresponding user information.
    3. Handled cases where the `name` parameter is missing or invalid, displaying appropriate messages when necessary.
    4. Implemented functionality to process a `color` parameter and apply the specified color to the page content if a valid color name is provided.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserExplorer.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly Dictionary<string, string> _users = new()
+        {
+            { "Oleg", "Oleg is a software developer from New York." },
+            { "Taras", "Taras is a graphic designer from San Francisco." },
+            { "Nikita", "Nikita is a data analyst from Chicago." }
+        };
+
+        [HttpGet]
+        public IActionResult Index(string name, string color)
+        {
+            string message;
+            if (string.IsNullOrEmpty(name))
+            {
+                message = "User name not specified.";
+            }
+            else if (_users.ContainsKey(name))
+            {
+                message = _users[name];
+            }
+            else
+            {
+                message = "User not found.";
+            }
+
+            ViewData["Message"] = message;
+            ViewData["Color"] = color ?? "black";
+
+            return View();
+        }
+    }
+}
